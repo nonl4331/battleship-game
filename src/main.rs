1ad80f5f09edb80f4a841f767246c76def1b5f41
nonl4331 <nicholasl0003@gmail.com>
2026-02-12 19:42:31 +1100

    moved more stuff to Board from main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use std::{
 
 use rand::random_bool;
 
+enum Status {
+    Won,
+    Loss,
+}
+
 #[derive(Debug)]
 struct Ship {
     pub pos: Vec<usize>,
@@ -50,6 +55,7 @@ struct Board {
     your_attacks: [u8; 100],
     enemy_attacks: [u8; 100],
     pending_attack: (u8, u8),
+    con: TcpStream,
 }
 
 impl Board {
@@ -57,15 +63,132 @@ impl Board {
     pub const HIT: u8 = 1;
     pub const SUNK: u8 = 2;
     pub const WIN: u8 = 3;
-    fn from_ships(ships: [Ship; 5]) -> Self {
-        Self {
-            ships,
+    fn play_game(con: TcpStream, host: bool) -> Status {
+        let mut ships: Vec<Ship> = Vec::new();
+        for (name, len) in [
+            ("Carrier", 5),
+            ("Battleship", 4),
+            ("Destroyer", 3),
+            ("Submarine", 3),
+            ("Patrol Boat", 2),
+        ] {
+            loop {
+                let x = get_pos(&format!(
+                    "please choose the x coordinate to put your {name} (length: {len}) [1-10]"
+                ));
+                let y = get_pos(&format!(
+                    "please choose the y coordinate to put your {name} (length: {len}) [1-10]"
+                ));
+
+                let mut input = String::new();
+                println!("Is your {name} (length: {len}) rotated? [y/n]");
+                std::io::stdin().read_line(&mut input).unwrap();
+                let down = match &input.trim().to_lowercase()[..] {
+                    "y" => true,
+
+                    "n" => false,
+                    i => {
+                        println!("Invalid input \"{i}\"! Please try again");
+                        continue;
+                    }
+                };
+
+                let Some(ship) = Ship::create_with_pos_and_rotation(x, y, len, down) else {
+                    println!("Invalid ship position, please try again.");
+                    continue;
+                };
+
+                if ships.iter().any(|s| s.intersects_ship(&ship)) {
+                    println!("{name} intersects existing ship. Please try again.");
+                    continue;
+                }
+
+                ships.push(ship);
+                break;
+            }
+        }
+        let mut b = Self {
+            ships: ships.try_into().unwrap(),
             your_attacks: [0; 100],
             enemy_attacks: [0; 100],
             pending_attack: (0, 0),
+            con,
+        };
+
+        // decide on which player goes first
+        let first;
+        if host {
+            first = random_bool(0.5);
+            b.con
+                .write_all(&[u8::from(!first)])
+                .expect("Failed to send move to server");
+        } else {
+            println!("Waiting for server to say who goes first.");
+            let arr: [u8; 1] = b
+                .con
+                .read_array()
+                .expect("Failed to get who goes first from server");
+            if arr[0] == 1 {
+                first = true;
+            } else if arr[0] == 0 {
+                first = false;
+            } else {
+                panic!("Server sent malformed data.");
+            }
+        }
+
+        // play first move if applicable:
+        if first {
+            println!("Our turn");
+            b.con.write_all(&[4]).unwrap();
+            b.make_move();
+        } else {
+            println!("Enemy turn");
+        }
+
+        loop {
+            let status: [u8; 1] = b
+                .con
+                .read_array()
+                .expect("Failed to get message from server");
+
+            match status[0] {
+                0 => {
+                    println!("Miss!");
+                    b.update_pending(2);
+                    Board::print_board(&b.your_attacks);
+                    b.receive_move();
+                    b.make_move();
+                } // miss, em, your move
+                1 => {
+                    println!("Hit!");
+                    b.update_pending(1);
+                    Board::print_board(&b.your_attacks);
+                    b.receive_move();
+                    b.make_move();
+                } // hit, em, your move
+                2 => {
+                    println!("Sunk!");
+                    b.update_pending(1);
+                    Board::print_board(&b.your_attacks);
+                    b.receive_move();
+                    b.make_move();
+                } // sunk, em, your move
+                3 => {
+                    println!("Sunk, you win!");
+                    b.update_pending(1);
+                    Board::print_board(&b.your_attacks);
+                    return Status::Won;
+                } // sunk, you win
+                4 => {
+                    b.receive_move();
+                    b.make_move();
+                } // em, first move
+                m => panic!("Invalid message from server: {m}"),
+            }
         }
     }
-    fn make_move(&mut self, con: &mut TcpStream) {
+    fn make_move(&mut self) {
         loop {
             let x = get_pos("Please choose the x coordinate of your attack [1-10]");
             let y = get_pos("Please choose the y coordinate of your attack [1-10]");
@@ -78,15 +201,19 @@ impl Board {
             }
             self.pending_attack = (x, y);
 
-            con.write_all(&[x, y])
+            self.con
+                .write_all(&[x, y])
                 .expect("Failed to send move to server");
             println!("sent: {:?}", [x, y]);
 
             break;
         }
     }
-    fn receive_move(&mut self, con: &mut TcpStream) {
-        let [x, y]: [u8; 2] = con.read_array().expect("Failed to get message from server");
+    fn receive_move(&mut self) {
+        let [x, y]: [u8; 2] = self
+            .con
+            .read_array()
+            .expect("Failed to get message from server");
 
         let mut hit = false;
         let mut sunk = false;
@@ -117,7 +244,7 @@ impl Board {
         }
         println!("Your ships:");
         Self::print_board(&self.enemy_attacks);
-        con.write_all(&[board_attack]).unwrap();
+        self.con.write_all(&[board_attack]).unwrap();
     }
     fn print_board(board: &[u8; 100]) {
         for line in 0..10 {
@@ -134,76 +261,9 @@ impl Board {
         }
     }
     fn update_pending(&mut self, attack_state: u8) {
-        self.your_attacks[((self.pending_attack.0 - 1) + (self.pending_attack.1 - 1) * 10) as usize] = attack_state;
-    }
-    fn main_loop(mut self, host: bool, mut con: TcpStream) -> () {
-    // decide on which player goes first
-    let first;
-    if host {
-        first = random_bool(0.5);
-        con.write_all(&[u8::from(!first)])
-            .expect("Failed to send move to server");
-    } else {
-        println!("Waiting for server to say who goes first.");
-        let arr: [u8; 1] = con
-            .read_array()
-            .expect("Failed to get who goes first from server");
-        if arr[0] == 1 {
-            first = true;
-        } else if arr[0] == 0 {
-            first = false;
-        } else {
-            panic!("Server sent malformed data.");
-        }
-    }
-
-    // play first move if applicable:
-    if first {
-        println!("Our turn");
-        con.write_all(&[4]).unwrap();
-        self.make_move(&mut con);
-    } else {
-        println!("Enemy turn");
-    }
-
-    loop {
-        let status: [u8; 1] = con.read_array().expect("Failed to get message from server");
-
-        match status[0] {
-            0 => {
-                println!("Miss!");
-                self.update_pending(2);
-                Board::print_board(&self.your_attacks);
-                self.receive_move(&mut con);
-                self.make_move(&mut con);
-            } // miss, em, your move
-            1 => {
-                println!("Hit!");
-                self.update_pending(1);
-                Board::print_board(&self.your_attacks);
-                self.receive_move(&mut con);
-                self.make_move(&mut con);
-            } // hit, em, your move
-            2 => {
-                println!("Sunk!");
-                self.update_pending(1);
-                Board::print_board(&self.your_attacks);
-                self.receive_move(&mut con);
-                self.make_move(&mut con);
-            } // sunk, em, your move
-            3 => {
-                println!("Sunk, you win!");
-                self.update_pending(1);
-                Board::print_board(&self.your_attacks);
-                break;
-            } // sunk, you win
-            4 => {
-                self.receive_move(&mut con);
-                self.make_move(&mut con);
-            } // em, first move
-            m => panic!("Invalid message from server: {m}"),
-        }
-    }
+        self.your_attacks
+            [((self.pending_attack.0 - 1) + (self.pending_attack.1 - 1) * 10) as usize] =
+            attack_state;
     }
 }
 
@@ -252,55 +312,7 @@ fn main() {
         }
     }
 
-    // create board
-    let mut ships: Vec<Ship> = Vec::new();
-    for (name, len) in [
-        ("Carrier", 5),
-        ("Battleship", 4),
-        ("Destroyer", 3),
-        ("Submarine", 3),
-        ("Patrol Boat", 2),
-    ] {
-        loop {
-            let x = get_pos(&format!(
-                "please choose the x coordinate to put your {name} (length: {len}) [1-10]"
-            ));
-            let y = get_pos(&format!(
-                "please choose the y coordinate to put your {name} (length: {len}) [1-10]"
-            ));
-
-            let mut input = String::new();
-            println!("Is your {name} (length: {len}) rotated? [y/n]");
-            stdin.read_line(&mut input).unwrap();
-            let down = match &input.trim().to_lowercase()[..] {
-                "y" => true,
-
-                "n" => false,
-                i => {
-                    println!("Invalid input \"{i}\"! Please try again");
-                    continue;
-                }
-            };
-
-            let Some(ship) = Ship::create_with_pos_and_rotation(x, y, len, down) else {
-                println!("Invalid ship position, please try again.");
-                continue;
-            };
-
-            if ships.iter().any(|s| s.intersects_ship(&ship)) {
-                println!("{name} intersects existing ship. Please try again.");
-                continue;
-            }
-
-            ships.push(ship);
-            break;
-        }
-    }
-
-    let board = Board::from_ships(ships.try_into().unwrap());
-
-
-    board.main_loop(host, con);
+    let _result = Board::play_game(con, host);
 }
 
 fn get_pos(msg: &str) -> u8 {
